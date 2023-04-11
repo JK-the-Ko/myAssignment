9adc01015cb8d4515844ef7a65fae8e5ff5eac4f
JK-the-Ko <rhworbs1124@naver.com>
2023-04-11 12:28:12 +0900

    Add 2 Unit Tests

--- testMyTranspose.py
+++ testMyTranspose.py
@@ -1,1 +1,33 @@
+import unittest
 
+import numpy as np
+
+from myTranspose import myTranspose
+
+
+class myTestCase(unittest.TestCase) :
+    def testTranspose1(self) :
+        # Generate Array
+        myVar1 = np.arange(1,11).reshape(2,5).transpose()
+        
+        # Run Inference
+        myVar1Pred = myTranspose(myVar1)
+        myVar1Target = myVar1.transpose()
+        
+        # Unit Test
+        self.assertTrue(np.testing.assert_equal(myVar1Pred, myVar1Target) is None)
+        
+    def testTranspose2(self) :
+        # Generate Array
+        myVar1 = np.ones(shape=(0,0))*np.nan
+        
+        # Run Inference
+        myVar1Pred = myTranspose(myVar1)
+        myVar1Target = np.empty(shape=(0,0))
+        
+        # Unit Test
+        self.assertTrue(np.testing.assert_equal(myVar1Pred, myVar1Target) is None)
+
+
+if __name__ == "__main__" :
+    unittest.main()
